dfa23eb447362c99a5b085cc2bb98e19d131eaf7
AgathePlunian <agathe.plunian@gmail.com>
2022-02-01 13:51:44 +0100

    rebug

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -5,14 +5,17 @@ const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_ke=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=Paris`)
     .then(res => res.json())
     .catch(err => console.log("Oh no", err))
 
 
 const retrieveWeatherForecastData = async (coordinates, isMocked) => {
     if (isMocked) {
+       
         return await _retrieveWeatherForecastMockedData()
+       
     }
+    console.log("sucess");
     return await _retrieveWeatherForecastApiData(coordinates)
 }
