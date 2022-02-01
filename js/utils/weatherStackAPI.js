947bf264baf4e7271d8b17ed7b1d688b09093236
AgathePlunian <agathe.plunian@gmail.com>
2022-02-01 14:10:28 +0100

    fix bug

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,11 +1,11 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = 'e673a3683fad91ccd5ba0b4c7e7e32e3'
 
 const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
 .then(res => res.json())
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
     .then(res => res.json())
     .catch(err => console.log("Oh no", err))
 
@@ -14,5 +14,9 @@ const retrieveWeatherForecastData = async (coordinates, isMocked) => {
     if (isMocked) {
         return await _retrieveWeatherForecastMockedData()
     }
-    return await _retrieveWeatherForecastApiData(coordinates)
+    else {
+        console.log("ok")
+        return await _retrieveWeatherForecastApiData(coordinates)
+    }
+
 }
